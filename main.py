a4572468e19dc779aa5b7d981c5818c714484e0f
David-McJonathan <162355249+David-McJonathan@users.noreply.github.com>
2024-03-05 18:56:13 +0100

    remove Cookie-Banner
    Open Bahn.de and reject cookies

--- main.py
+++ main.py
@@ -3,23 +3,64 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
-import time
-import datetime
-
 
 def start():
 
-   print("ToDo: start")
+
+    print("Welcome to the search for the best train price")
+
+
+    depature = "Hamburg Hbf"
+    destination = "Frankfurt Hbf"
+    date = ""
+
+    options = ChromeOptions()
+#    options.add_argument("--headless=new")
+#    options.add_argument("--window-size=1920,1080")
+#    driver.get_screenshot_as_file("screenshot.png")
+    
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://www.bahn.de/")
+    driver.implicitly_wait(1.5)
+       
+    
+    removeCookieBanner(driver)
+    openBahnStartseite(driver)
+
+
+    changeOpen(driver)
+
+    setDate(driver, date)
+    setDeparture(driver, depature)        
+    setDestination(driver, destination)
+
+    changeDone(driver)
+
+    getBestpreis(driver)
+  
+
+
+    
+
 
 
 def removeCookieBanner(driver):
 
-    print("ToDo: removeCookieBanner")
+    time.sleep(2)
+
+    shadow_host = driver.find_element(By.TAG_NAME, "div")
+    shadow_root = shadow_host.shadow_root
+    shadow_content = shadow_root.find_element(By.CLASS_NAME, 'btn.btn--secondary.js-accept-essential-cookies')
+
+    shadow_content.click()
+
+
 
 
 def openBahnStartseite (driver):
 
-    print("ToDo: openBahnStartseite")    
+    print("ToDo: openBahnStartseite")
+   
 
 def changeOpen(driver):
     
@@ -46,14 +87,19 @@ def setDestination(driver, city):
    print("ToDo: setDestination")
 
 
+
 def getBestpreis(driver):
-  
-   print("ToDo: getBestpreis")
+
+    print("ToDo: getBestpreis")
+
 
 
 def convertElementToFloat(str):
-  
-   print("ToDo: convertElementToFloat")
+
+    print("ToDo: convertElementToFloat")
+
+   
+
 
 
 
