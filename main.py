00283f7ada2e071f72c1b8b3c6845e6c59368d51
David-McJonathan <162355249+David-McJonathan@users.noreply.github.com>
2024-03-05 18:41:54 +0100

    Create main.py
    Basic structure of the project.
    Setting up all expected functions.

---
+++ main.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+import time
+import datetime
+
+
+def start():
+
+   print("ToDo: start")
+
+
+def removeCookieBanner(driver):
+
+    print("ToDo: removeCookieBanner")
+
+
+def openBahnStartseite (driver):
+
+    print("ToDo: openBahnStartseite")    
+
+def changeOpen(driver):
+    
+    print("ToDo: changeOpen")
+
+
+def changeDone(driver):
+
+    print("ToDo: changeDone")    
+
+
+def setDate(driver, date):
+
+    print("ToDo: setDate")
+
+
+def setDeparture(driver, city):
+
+    print("ToDo: setDeparture")
+
+
+def setDestination(driver, city):
+
+   print("ToDo: setDestination")
+
+
+def getBestpreis(driver):
+  
+   print("ToDo: getBestpreis")
+
+
+def convertElementToFloat(str):
+  
+   print("ToDo: convertElementToFloat")
+
+
+
+if __name__ == "__main__":
+    start()
+
+
+
+
+
+
+
+
+
+
+
+
